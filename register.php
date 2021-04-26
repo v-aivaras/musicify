9df907e6b369b6602257bb05e148a8c4e18bf1b8
v-aivaras <aivaras.valasinas@gmail.com>
2021-04-26 17:28:33 +0100

    Added right column content + style to register.php

--- register.php
+++ register.php
@@ -58,7 +58,7 @@
                 <p>
                     <?php echo $account->getError(Constants::$loginFailed); ?>
                     <label for="loginUsername">Username</label>
-                    <input type="text" id="loginUsername" name="loginUsername" placeholder="e.g. JohnSmith" required>
+                    <input type="text" id="loginUsername" name="loginUsername" placeholder="e.g. JohnSmith"  value="<?=getInputValue('loginUsername') ?>" required>
                 </p>
                 <p>
                     <label for="loginPassword">Password</label>
@@ -122,6 +122,16 @@
                 </div>
             </form>
 
+        </div>
+
+        <div id="loginText">
+            <h1>Get great music, right now!</h1>
+            <h2>Listen to loads of songs for free</h2>
+            <ul>
+                <li>Discover music you'll fall in love with</li>
+                <li>Create your palylists</li>
+                <li>Follow artists to keep up to date</li>
+            </ul>
         </div>
     </div>
 
