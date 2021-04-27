c1c1830b69bbeab2881bff08ff0aa7fcdf0759ed
v-aivaras <aivaras.valasinas@gmail.com>
2021-04-27 19:40:56 +0100

    Added main player control buttons

--- index.php
+++ index.php
@@ -19,8 +19,66 @@
 
 </head>
 <body>
-    <h3>Hello <?=$userLoggedIn ?></h3>
-    <a href="logout.php">Log out</a>
+    <!-- <h3>Hello <?=$userLoggedIn ?></h3>
+    <a href="logout.php">Log out</a> -->
+
+
+    <div id="nowPlayingBarContainer">
+        <div id="nowPlayingBar">
+            
+            <div id="nowPlayingLeft">
+
+            </div>
+
+            <div id="nowPlayingCenter">
+                
+                <div class="content playerControls">
+
+                    <div class="buttons">
+
+                        <button class="controlButton shuffle" title="Shuffle">
+                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                        </button>
+
+                        <button class="controlButton previous" title="Previous">
+                            <img src="assets/images/icons/previous.png" alt="Previous">
+                        </button>
+
+                        <button class="controlButton play" title="Play">
+                            <img src="assets/images/icons/play.png" alt="Play">
+                        </button>
+
+                        <button class="controlButton pause" title="Pause" style="display:none">
+                            <img src="assets/images/icons/pause.png" alt="Pause">
+                        </button>
+
+                        <button class="controlButton next" title="Next">
+                            <img src="assets/images/icons/next.png" alt="Next">
+                        </button>
+
+                        <button class="controlButton repeat" title="Repeat">
+                            <img src="assets/images/icons/repeat.png" alt="Repeat">
+                        </button>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div id="nowPlayingRight">
+                
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
     <script src="assets/js/script.js"></script>
 </body>
 </html>
